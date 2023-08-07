49751ef7528ec9da440a2a481443002aff382607
Gaus Al Munir Tushar <tushar01910533370@gmail.com>
2023-08-07 17:47:31 +0600

    array, de structuring

---
+++ array.js
@@ -0,0 +1,17 @@
+// simple array
+let MyArray = new Array ()
+let myArray = []
+
+
+// Multidimentional Array
+let bangladesh = ["Dhaka", "Rangpur", "Cumilla"];
+let india = ["Kolkata", "New Delhi", "Pune"];
+let china = ["Uhan", "Shagahi"];
+
+let asia = [bangladesh, india, china]
+console.log(asia[1][1])
+
+// Array de structuring
+let countries = ["BAN", "IND", "PAK"]
+let [a,b,c] = countries;
+console.log(b)
